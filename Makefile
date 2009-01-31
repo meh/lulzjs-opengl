a918428bd795b5184994d1692a31ae2bc866fbaf
meh <meh.ffff@gmail.com>
2009-01-31 14:05:58 +0100

    Starting thingy

---
+++ Makefile
@@ -0,0 +1,48 @@
+VERSION = 0.1
+
+CC         = gcc
+CXX        = g++
+BINDIR     = /usr/bin
+LJS_LIBDIR = /usr/lib/lulzjs
+CFLAGS     = -Os -DXP_UNIX -DJS_THREADSAFE -D__LIB_VERSION__="\"${VERSION}\"" -I/usr/include/js -I./src
+LDFLAGS    = -ljs -llulzjs
+
+ifdef DEBUG
+CFLAGS += -g
+endif
+
+ifdef DDEBUG
+CFLAGS += -DDEBUG -g
+endif
+
+LIB_DIR     = src
+LIB         = ${LIB_DIR}/OpenGL.o
+LIB_CFLAGS  = ${CFLAGS}
+LIB_LDFLAGS = ${LDFLAGS} -lX11 -lXi -lXmu -lglut -lGL -lGLU -lm 
+
+all: lib
+
+lib: $(LIB)
+
+$(LIB) : $(LIB:.o=.c)
+	${CC} ${LIB_CFLAGS} -fPIC -c $*.c -o $*.lo
+	${CC} ${LIB_LDFLAGS} -shared -Wl,-soname,`basename $*`.so -o $*.o $*.lo -lc
+
+lib_install: lib
+	mkdir -p ${LJS_LIBDIR}/OpenGL
+########
+	cp -f  ${LIB_DIR}/init.js		${LJS_LIBDIR}/OpenGL/init.js
+	cp -f  ${LIB_DIR}/OpenGL.o		${LJS_LIBDIR}/OpenGL/OpenGL.so
+	cp -f  ${LIB_DIR}/OpenGL.js		${LJS_LIBDIR}/OpenGL/OpenGL.js
+#######
+	chmod -R a+rx ${LJS_LIBDIR}/OpenGL
+
+lib_uninstall:
+	rm -rf ${LJS_LIBDIR}/OpenGL
+
+
+install: lib_install
+uninstall: lib_uninstall
+
+clean:
+	find src|egrep "\.l?o"|xargs rm -f
---
+++ src/init.js
@@ -0,0 +1,20 @@
+/****************************************************************************
+* This file is part of lulzJS-OpenGL                                        *
+* Copyleft meh.                                                             *
+*                                                                           *
+* lulzJS-OpenGL is free software: you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation, either version 3 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* lulzJS-OpenGL is distributed in the hope that it will be useful.          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty o.            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See th.             *
+* GNU General Public License for more details.                              *
+*                                                                           *
+* You should have received a copy of the GNU General Public License         *
+* along with lulzJS-OpenGL.  If not, see <http://www.gnu.org/licenses/>.    *
+****************************************************************************/
+
+require(["OpenGL.so", "OpenGL.js"]);
+
