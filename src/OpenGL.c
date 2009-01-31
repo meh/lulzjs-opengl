e6303afcd53dce520dbd92bb3ae4de4791350974
meh <meh.ffff@gmail.com>
2009-01-31 14:09:48 +0100

    Forgot these lol

---
+++ src/OpenGL.c
@@ -0,0 +1,43 @@
+/****************************************************************************
+* This file is part of lulzJS-OpenGL                                        *
+* Copyleft meh.                                                             *
+*                                                                           *
+* lulzJS-OpenGL is free software: you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation, either version 3 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* lulzJS-OpenGL is distributed in the hope that it will be useful.          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty o.            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See th.             *
+* GNU General Public License for more details.                              *
+*                                                                           *
+* You should have received a copy of the GNU General Public License         *
+* along with lulzJS-OpenGL.  If not, see <http://www.gnu.org/licenses/>.    *
+****************************************************************************/
+
+#include "OpenGL.h"
+
+JSBool exec (JSContext* cx) { return OpenGL_initialize(cx); }
+
+JSBool
+OpenGL_initialize (JSContext* cx)
+{
+    JSObject* object = JS_DefineObject(
+        cx, JS_GetGlobalObject(cx),
+        OpenGL_class.name, &OpenGL_class, NULL, 
+        JSPROP_PERMANENT|JSPROP_READONLY|JSPROP_ENUMERATE
+    );
+
+    if (object) {
+        JS_DefineFunctions(cx, object, OpenGL_methods);
+
+        jsval property;
+
+        return JS_TRUE;
+    }
+
+    return JS_FALSE;
+}
+
+
