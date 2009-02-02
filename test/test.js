2cdeecd091e679b75e012eeae851537f7b91976e
meh <meh.ffff@gmail.com>
2009-02-02 04:56:36 +0100

    Change in the test.

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ require("OpenGL/GLUT");
 
 GLUT.init(arguments, GLUT.DisplayModes.Double|GLUT.DisplayModes.RGB|GLUT.DisplayModes.Dept);
 
-var window = new GLUT.Window({title: "lulzJS OpenGL test", width: 800, height: 600}, {
+var window = new GLUT.Window({title: "lulzJS OpenGL test", width: 640, height: 480}, {
     onDisplay: function () {
         GL.clear(GL.Buffers.Color|GL.Buffers.Depth);
         GL.flush();
